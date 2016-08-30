c7905749ce5f8afe7961ede496edac2e35335f22
Ron Olson <rolson@lbpc.com>
2016-08-30 16:20:32 -0500

    Made extension Swift 3 compatible

--- Crazy8s/extensions.swift
+++ Crazy8s/extensions.swift
@@ -3,12 +3,12 @@ import Foundation
 
 extension MutableCollectionType where Index == Int {
 	/// Shuffle the elements of `self` in-place.
-	mutating func shuffleInPlace() {
+	mutating func shuffle() {
 		// empty and single-element collections don't shuffle
 		if count < 2 { return }
 
-		for i in 0 ..< count - 1 {
-			let j = Int(arc4random_uniform(UInt32(count - i))) + i
+		for i in startIndex ..< endIndex - 1 {
+			let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
 			guard i != j else { continue }
 			swap(&self[i], &self[j])
 		}
