ec1827de372164f5e135fa999a01167c660acdb8
Ron Olson <rolson@lbpc.com>
2016-08-31 09:29:05 -0500

    Made more linux friendly

--- Crazy8s/extensions.swift
+++ Crazy8s/extensions.swift
@@ -1,20 +1,32 @@
 
 import Foundation
 
-extension MutableCollectionType where Index == Int {
-	/// Shuffle the elements of `self` in-place.
-	mutating func shuffle() {
-		// empty and single-element collections don't shuffle
-		if count < 2 { return }
+#if os(Linux)
+	extension MutableCollection where Index == Int {
+		/// Shuffle the elements of `self` in-place.
+		mutating func shuffle() {
+			// empty and single-element collections don't shuffle
+			if count < 2 { return }
 
-		for i in startIndex ..< endIndex - 1 {
-			#if os(Linux)
+			for i in startIndex ..< endIndex - 1 {
 				let j = Int(random() % (endIndex - i)) + i
-			#else
-				let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
-			#endif
-			guard i != j else { continue }
-			swap(&self[i], &self[j])
+				guard i != j else { continue }
+				swap(&self[i], &self[j])
+			}
 		}
 	}
-}
+#else
+	extension MutableCollectionType where Index == Int {
+		/// Shuffle the elements of `self` in-place.
+		mutating func shuffle() {
+			// empty and single-element collections don't shuffle
+			if count < 2 { return }
+
+			for i in startIndex ..< endIndex - 1 {
+				let j = Int(arc4random_uniform(UInt32(endIndex - i))) + i
+				guard i != j else { continue }
+				swap(&self[i], &self[j])
+			}
+		}
+	}
+#endif
