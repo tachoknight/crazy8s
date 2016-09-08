5bb896e169555b59b9c4e298774c2f38088134fc
Ron Olson <rolson@lbpc.com>
2016-09-08 14:45:13 -0500

    Updated to Swift 3 via XCode 8’s migration tool
    ToDo: Remove unnecessary #if os(Linux) where there are no differences between it and macOS.

--- Crazy8s/extensions.swift
+++ Crazy8s/extensions.swift
@@ -16,7 +16,7 @@ import Foundation
 		}
 	}
 #else
-	extension MutableCollectionType where Index == Int {
+	extension MutableCollection where Index == Int {
 		/// Shuffle the elements of `self` in-place.
 		mutating func shuffle() {
 			// empty and single-element collections don't shuffle
@@ -48,40 +48,24 @@ import Foundation
 
 			return mySortedKeys
 		}
-
-		/*
-		 // Faster because of no lookups, may take more memory because of duplicating contents
-		 func keysSortedByValue(isOrderedBefore: (Value, Value) -> Bool) -> [Key] {
-		 return Array(self)
-		 .sort() {
-		 let (_, lv) = $0
-		 let (_, rv) = $1
-		 return isOrderedBefore(lv, rv)
-		 }
-		 .map {
-		 let (k, _) = $0
-		 return k
-		 }
-		 }
-		 */
-	}
+    }
 #else
 	extension Dictionary {
-		func sortedKeys(isOrderedBefore: (Key, Key) -> Bool) -> [Key] {
-			return Array(self.keys).sort(isOrderedBefore)
+		func sortedKeys(_ isOrderedBefore: (Key, Key) -> Bool) -> [Key] {
+			return Array(self.keys).sorted(by: isOrderedBefore)
 		}
 
 		// Slower because of a lot of lookups, but probably takes less memory (this is equivalent to Pascals answer in an generic extension)
-		func sortedKeysByValue(isOrderedBefore: (Value, Value) -> Bool) -> [Key] {
+		func sortedKeysByValue(_ isOrderedBefore: (Value, Value) -> Bool) -> [Key] {
 			return sortedKeys {
 				isOrderedBefore(self[$0]!, self[$1]!)
 			}
 		}
 
 		// Faster because of no lookups, may take more memory because of duplicating contents
-		func keysSortedByValue(isOrderedBefore: (Value, Value) -> Bool) -> [Key] {
+		func keysSortedByValue(_ isOrderedBefore: (Value, Value) -> Bool) -> [Key] {
 			return Array(self)
-				.sort() {
+				.sorted() {
 					let (_, lv) = $0
 					let (_, rv) = $1
 					return isOrderedBefore(lv, rv)
